879d1284c216924cebde13597d38828ed8b9cdae
afik <k.afis3@rocketmail.com>
2015-10-27 16:57:31 +0700

    test

---
+++ src/jgroups/test/SetTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jgroups.test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SetTest {
+    @Test
+    public void testSet() {
+        
+    }
+    
+}
